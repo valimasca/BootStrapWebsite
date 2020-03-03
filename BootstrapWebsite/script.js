43a4f2afd3a5833253fbb23a837e2e16f1ffd145
Vali Masca <vali.masca@Vali-Masca-MBP.local>
2020-03-02 16:50:11 -0800

    first commit

---
+++ BootstrapWebsite/script.js
@@ -0,0 +1,52 @@
+// //Time out function
+// const thingToDoWhenTimeoutIsOver = () => alert('Timeout completed');
+// setTimeout(thingToDoWhenTimeoutIsOver, 5000);
+
+// //Another timeout function
+// const later = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+//             later(1000)
+//                 .then(() => {
+//                 console.log('After 1 second');
+//                 return later(1000);
+//              })
+//              .then(() => {
+//                 console.log('After 2 second');
+//                 return later(1000);
+//                  })
+//             .then(() => {
+//                 console.log('After 3 second');
+//                 return later(1000);
+//             })
+//             .then(() => {
+//                 console.log('Vali is amazing');
+//                 return later(2000);
+//             })
+
+
+// // fetch a URL
+// fetch("https://api.github.com/search/repositories?q=react")
+// .then(response => response.json())
+// .then(json => console.log(json))
+// .catch(err => console.log("Error fetching query"));
+
+
+//Bootstrap form
+ // Self-executing function
+ (function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
