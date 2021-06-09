893994d53b3376d0f3c69072e72f193be7ddecec
valimasca <e3skadron@gmail.com>
2021-06-09 09:29:31 -0700

    push last

---
+++ My First Website/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(".cardSocialMedia").hover(function () {
+        $(this).css("font-size", "60px");
+    });
+});
+
+
+
+function loadRepos() {
+    let repo = new XMLHttpRequest();
+    repo.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let repObj = JSON.parse(this.responseText);
+            document.getElementById("name0").innerHTML = repObj[0].name;
+            document.getElementById("name1").innerHTML = repObj[1].name;
+            document.getElementById("name2").innerHTML = repObj[2].name;
+            document.getElementById("name3").innerHTML = repObj[3].name;
+            document.getElementById("name4").innerHTML = repObj[4].name;
+        }
+    };
+    repo.open("GET", "https://api.github.com/users/valimasca/repos", true);
+    repo.send();
+}
